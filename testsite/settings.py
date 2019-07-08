166daf4bb24e0dc3a10d0145bdeaa722dc63da30
PhysTom88 <tom.ravenscroft@uktv.co.uk>
2019-07-08 09:11:08 +0100

    change the database engine

--- testsite/settings.py
+++ testsite/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'testsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
     }
 }
